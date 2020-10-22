f19a79a07842b378473384f8beadc7f06d9b468c
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-10-21 23:56:15 -0700

    changes to yaml

--- CloudComputing/PJ2/photoapp/__init__.py
+++ CloudComputing/PJ2/photoapp/__init__.py
@@ -8,8 +8,8 @@ import os
 CLOUD_STORAGE_BUCKET = os.environ.get("CLOUD_STORAGE_BUCKET")
 CLOUD_THUMBNAIL_BUCKET = os.environ.get("CLOUD_THUMBNAIL_BUCKET")
 print(CLOUD_STORAGE_BUCKET)
-# if CLOUD_STORAGE_BUCKET is None:
-#     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Project-1f0d49812708.json"
+if CLOUD_STORAGE_BUCKET is None:
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Project-1f0d49812708.json"
     # os.environ["CLOUD_STORAGE_BUCKET"] = "papus"
 CLOUD_STORAGE_BUCKET = "papus" #os.environ.get("CLOUD_STORAGE_BUCKET")
 CLOUD_THUMBNAIL_BUCKET = "thumbnails-papus"
