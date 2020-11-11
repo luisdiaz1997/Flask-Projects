4cadf8ce4097e623cfd163a12cd4630b828d3355
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-11-10 23:09:47 -0800

    added entry points

--- CloudComputing/GP/backend/app.py
+++ CloudComputing/GP/backend/app.py
@@ -1,8 +1,12 @@
 import base64
-from flask import Flask, send_file, request, jsonify, make_response
+from flask import Flask, send_file, request, jsonify, make_response, send_file
 from flask_cors import CORS
 
 from audio import Audio_API
+
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 CORS(app)
@@ -34,7 +38,46 @@ def process_image():
 def process_audio():
     audio_obj= Audio_API()
     response = audio_obj.analyze_audio(request)
-    return response    
+    return response  
+
+@app.route('/analyze_text', methods=["GET", "POST"])#translate Sally
+def process_text():
+    if request.method=="POST":
+        print(request.form)
+        print(request.files)
+
+        response_body = {
+                "message": "Hola mi nombre es Luis"
+            }
+        response = jsonify(response_body)
+        return response
+
+@app.route('/text_to_audio',  methods=["GET", "POST"]) #Sally
+def text_to_audio():
+    if request.method=="POST":
+        #If request doesnt have id, then add it to db
+        # If it does, send text to api     
+        response_body = {
+                "message": "Hola mi nombre es Luis"
+            }
+        file_path = os.path.join(dir_path, 'test.mp3')
+        return send_file(file_path)
+
+@app.route('/get_quotes', methods=["GET", "POST"]) #Bhavani
+def get_quotes():
+    if request.method=="GET":
+
+        response_body = [{
+            "id": "2",
+            "text": "This world is beautiful",
+        },{
+            "id": "3",
+            "text": "This world is ugly",
+        }]
+
+        response = jsonify(response_body)
+        return response
+
 
 if __name__=='__main__':
     app.run(port=5000, debug=True)
--- CloudComputing/GP/frontend/src/views/Home.vue
+++ CloudComputing/GP/frontend/src/views/Home.vue
@@ -6,6 +6,8 @@
       <AudioUploader/>
       <TextUploader/>
     </b-container>
+
+    <b-button @click="get_audio"> Click me to get audio</b-button>
   </div>
 </template>
 
@@ -15,7 +17,7 @@ import ImageUploader from '@/components/ImageUploader.vue'
 import AudioUploader from '@/components/AudioUploader.vue'
 import TextUploader from '@/components/TextUploader.vue'
 
-
+import axios from "axios"
 export default {
   name: 'Home',
   components: {
@@ -28,6 +30,20 @@ export default {
     updateImage(newImage){
       this.image=newImage
       this.$store.commit('replaceImage', this.image)
+    },
+    get_audio()
+    {
+      let formData = new FormData();
+      formData.append('text', 'Hello universe');
+      let config=  { 
+          headers: {
+          'Content-Type': 'multipart/form-data'
+          }
+      }
+      axios.post('http://127.0.0.1:5000/text_to_audio', formData, config).then((response)=>{
+          console.log(response)
+          
+          })
     }
   },
   data(){
